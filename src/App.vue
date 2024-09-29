7666e4ced979b45d58488c8ae9eb4cf302e266b9
felipespaniol <fespaniol@gmail.com>
2024-09-29 20:13:23 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="editMessage">Edit Text</button>
+    <button v-if="message === 'Text has been updated!'" @click="resetMessage">Reset Text</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditMessage',
+  data() {
+    return {
+      message: 'Original text'
+    };
+  },
+  methods: {
+    editMessage() {
+      this.message = 'Text has been updated!';
+    },
+    resetMessage() {
+      this.message = '';
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+h1 {
+  color: #000;
+}
+</style>
